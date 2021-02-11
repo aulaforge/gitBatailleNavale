6be3a5991380e7b08ba1403498165e045e208a1f
aulaforge <aurelien.laforge@ensta-paris.fr>
2021-02-11 19:21:11 +0100

    Exercice 1 complété

--- Board.java
+++ Board.java
@@ -36,38 +36,92 @@ class Board{
     }
 
     public void print(){
-      int n=navires.length;
-      if (n<10){
-        System.out.print(" ");
-      }
-      else if(n>=10 && n<100){
-        System.out.print("  ");
-      }
-      else{
-        System.out.println("Taille trop grande, non gérée, veuillez rentrer une valeur inférieure à 100");
+      int taille=navires.length;
+      int nombreCaracParLigne;
+      if(taille>=27){
+        System.out.println("Taille trop grande, non gérée, veuillez rentrer une valeur entre 4 et 26 (bornes incluses).");
         return;
       }
-      for (int i=0; i<n;i++){
+      else if(taille<4){
+        System.out.println("Taille trop petite, non gérée, veuillez rentrer une valeur entre 4 et 26 (bornes incluses).");
+        return;
+      }
+      if (taille<10){
+        nombreCaracParLigne=2*taille+1;
+      }
+      else{
+        nombreCaracParLigne=2*taille+2;
+      }
+
+      System.out.print("Navires :");
+      for(int gap=0;gap<(nombreCaracParLigne-5);gap++){
+        System.out.print(" ");
+      }
+      System.out.println("Frappes :");
+
+      if (taille<10){
+        System.out.print(" ");
+      }
+      else{
+        System.out.print("  ");
+      }
+
+      for (int i=0; i<taille;i++){
+        char c=(char)(65+i);
+        System.out.print(" " + c);
+      }
+
+      System.out.print("    ");
+
+      if (taille<10){
+        System.out.print(" ");
+      }
+      else{
+        System.out.print("  ");
+      }
+
+      for (int i=0; i<taille;i++){
         char c=(char)(65+i);
         System.out.print(" " + c);
       }
       System.out.println(" ");
-      for(int j=1; j<n+1; j++){
-        if (j<10){
+      for(int j=1; j<taille+1; j++){
+        if (j<10&&taille>=10){
           System.out.print(j + " ");
         }
         else{
           System.out.print(j);
         }
-        for(int i=0;i<n; i++){
+        for(int i=0;i<taille; i++){
           System.out.print(" " + navires[j-1][i]);
         }
+
+        System.out.print("    ");
+
+        if (j<10&&taille>=10){
+          System.out.print(j + " ");
+        }
+        else{
+          System.out.print(j);
+        }
+        for(int i=0;i<taille; i++){
+          char affichageFrappe;
+          if(frappes[j-1][i]){
+            affichageFrappe='x';
+          }
+          else{
+            affichageFrappe='.';
+          }
+          System.out.print(" " + affichageFrappe);
+        }
+
+
         System.out.println(" ");
       }
     }
 
     public static void main(String[] args) {
-      Board test=new Board("Test");
+      Board test=new Board("Test", 12);
       test.print();
     }
 
